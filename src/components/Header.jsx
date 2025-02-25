c4a3ece4c076350d9da573d5c73980c369abe29c
Musti <safi.najjar1@gmail.com>
2025-02-25 06:53:55 +0100

    food-order app initialize project

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src="" alt="" />
+        <h1></h1>
+      </div>
+      <nav>
+        <button>Cart (0)</button>
+      </nav>
+    </header>
+  );
+}
