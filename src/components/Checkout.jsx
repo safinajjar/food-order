c58915c9841c4df5a1d2eb1b429ef06c664772a8
Musti <safi.najjar1@gmail.com>
2025-02-27 07:26:00 +0100

    add order handling in Checkout component; implement form submission with mutation for creating orders

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -5,6 +5,8 @@ import { currencyFormatter } from "../../utils/formatting";
 import Input from "./UI/Input";
 import Button from "./UI/Button";
 import UserProgressContext from "../store/UserProgressContext";
+import { useMutation } from "@tanstack/react-query";
+import { BASE_URL } from "../main";
 
 export default function Checkout() {
   const cartCtx = use(CartContext);
@@ -14,17 +16,48 @@ export default function Checkout() {
     return totalPrice + item.quantity * item.price;
   }, 0);
 
+  const mutation = useMutation({
+    mutationFn: async (formData) => {
+      const response = await fetch(`${BASE_URL}/orders`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to create user");
+      }
+
+      return response.json();
+    },
+  });
+
   const handleClose = () => {
     userProgressCtx.hideCheckout();
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const customerData = Object.fromEntries(
+      new FormData(event.target).entries()
+    );
+
+    mutation.mutate({
+      order: {
+        items: cartCtx.items,
+        customer: customerData,
+      },
+    });
+  };
+
   return (
     <Modal open={userProgressCtx.progress === "checkout"}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
-        <Input label="Full Name" type="text" id="fullName" />
+        <Input label="Full Name" type="text" id="name" />
         <Input label="E-Mail Address" type="email" id="email" />
         <Input label="Street" type="text" id="street" />
 
